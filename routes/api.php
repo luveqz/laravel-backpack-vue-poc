b14a656334b3cb7eb92572409f464152c7319c1f
Luis Velásquez <luvejo.dev@gmail.com>
2024-06-15 20:37:23 -0500

    feat: add Task model, factory and endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tasks', function() {
+    return Task::all();
+});
 
 
 Route::get('/user', function (Request $request) {
